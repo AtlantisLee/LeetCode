1e7934ccf0ae66c92ac80c7256fa656724e7ee69
AtlantisLee <noah.atlantis.lee@hotmail.com>
2018-12-02 10:31:57 +0800

    Edit gitignore

---
+++ Array/src/eazy/Solution1.java
@@ -0,0 +1,56 @@
+package eazy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Copyright (c) 2018
+ * two-sum
+ * @Version: Since 1.0.0
+ * @Author: Atlantis
+ * @Date: 2018/12/2 10:12
+ */
+public class Solution1 {
+    public int[] twoSum01(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No solution");
+    }
+
+    public int[] twoSum02(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No solution");
+    }
+
+    public int[] twoSum03(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 9, 11};
+        int target = 9;
+        System.out.println(new Solution1().twoSum01(nums, target));
+    }
+}
